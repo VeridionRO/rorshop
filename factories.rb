72399359596217dcc10c0ae165006cf3c8375342
Mihai Vinaga <vinaga.mihai@gmail.com>
2014-03-22 14:00:53 +0200

    Added factories for many-to-many products to categories

--- factories.rb
+++ factories.rb
@@ -5,11 +5,10 @@ FactoryGirl.define do
     description Faker::Lorem.paragraph
   end
 
-
   factory :valid_image, class: Image do
-    title      Faker::Commerce.department
-    uri        "Product01_resized.png"
-    product    FactoryGirl.create(:valid_product)
+    title   Faker::Commerce.department
+    uri     "Product01_resized.png"
+    product FactoryGirl.create(:valid_product)
   end
 
   factory :image, class: Image do
@@ -26,15 +25,51 @@ FactoryGirl.define do
   factory :product, class: Product do
     name        Faker::Commerce.product_name
     description Faker::Lorem.paragraph
+  end
 
-    factory :product_with_images do
-      ignore do
-        posts_count 1
-      end
+  factory :product_with_images, parent: :product do
+    ignore do
+      image { FactoryGirl.create(:image) }
+    end
 
-      after(:create) do |product, evaluator|
-        FactoryGirl.create_list(:image, evaluator.posts_count, product: product)
-      end
+    after(:create) do |product, evaluator|
+      product.images << evaluator.image
+    end
+  end
+
+  factory :product_category do
+    association :product
+    association :category
+  end
+
+  factory :product_with_category, parent: :product do
+    ignore do
+      category_count 1
+    end
+    after(:create) do |product, evaluator|
+      FactoryGirl.create_list(:product_category, evaluator.category_count, product: product)
+    end
+  end
+
+  # c = FactoryGirl.create(:category_with_product, products_count: 10)
+  factory :category_with_product, parent: :category do
+    ignore do
+      products_count 1
+    end
+    after(:create) do |category, evaluator|
+      FactoryGirl.create_list(:product, evaluator.products_count,
+        categories: [category])
+    end
+  end
+
+  # p = FactoryGirl.create(:product_with_categories, categories_count: 10)
+  factory :product_with_categories, parent: :product do
+    ignore do
+      categories_count 1
+    end
+    after(:create) do |product, evaluator|
+      FactoryGirl.create_list(:category, evaluator.categories_count,
+        products: [product])
     end
   end
 
