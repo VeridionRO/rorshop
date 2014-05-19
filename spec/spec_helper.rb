9829ff07c6ccf6a5afea46c1da15d34fac5c054e
Mihai Vinaga <vinaga.mihai@gmail.com>
2014-05-19 16:56:28 +0300

    Heroku

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,6 @@ require 'spork'
 require 'capybara'
 require 'capybara/rspec'
 
-Capybara.javascript_driver = :webkit
-
 Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
@@ -20,7 +18,6 @@ Spork.prefork do
     config.use_transactional_fixtures = true
     config.infer_base_class_for_anonymous_controllers = false
     config.order = "random"
-    config.include Capybara::DSL, :type => :request
   end
 end
 
