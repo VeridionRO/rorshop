e7e6a98a22ed663245695085ede907a5c6760d30
Mihai Vinaga <vinaga.mihai@gmail.com>
2014-05-19 17:48:24 +0300

    Heroku

--- db/migrate/20140309143403_add_products_ref_to_image.rb
+++ db/migrate/20140309143403_add_products_ref_to_image.rb
@@ -1,6 +1,4 @@
 class AddProductsRefToImage < ActiveRecord::Migration
   def change
-    add_reference :images, :product, index: true, 
-      after: :id, null: false
   end
 end
--- db/migrate/20140309161917_add_default_image_to_image.rb
+++ db/migrate/20140309161917_add_default_image_to_image.rb
@@ -1,5 +1,4 @@
 class AddDefaultImageToImage < ActiveRecord::Migration
   def change
-    add_column :images, :default_image, :bool, default: true, after: :product_id
   end
 end
